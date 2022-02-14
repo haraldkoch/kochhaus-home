33218055adc9217d4236ed2c62e22281f94ef291
Harald Koch <chk@anemone.chk.cfrq.net>
2022-02-14 08:05:14 -0500

    typo

--- cluster/apps/default/ceph-test/backup.sh
+++ cluster/apps/default/ceph-test/backup.sh
@@ -2,4 +2,4 @@
 
 CURRENT_DATE=$(date +"%FT%H%M")
 
-mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS --quote-names --databases $DB_DATABASE >/backup/${DB_DATABASE}.${CURRENT_DATE}.sql"
+mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS --quote-names --databases $DB_DATABASE >/backup/${DB_DATABASE}.${CURRENT_DATE}.sql
