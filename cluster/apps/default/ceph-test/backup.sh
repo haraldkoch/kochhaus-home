7facb20e3efc39f36a6c6e9ecdc0fb9f5f049d65
Harald Koch <harald.koch@gmail.com>
2022-02-14 09:25:59 -0500

    backup debug

--- cluster/apps/default/ceph-test/backup.sh
+++ cluster/apps/default/ceph-test/backup.sh
@@ -2,4 +2,8 @@
 
 CURRENT_DATE=$(date +"%FT%H%M")
 
+echo "running: mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS --quote-names --databases $DB_DATABASE \>/backup/${DB_DATABASE}.${CURRENT_DATE}.sql"
 mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS --quote-names --databases $DB_DATABASE >/backup/${DB_DATABASE}.${CURRENT_DATE}.sql
+
+echo "Complete."
+/bin/ls -l /backup
