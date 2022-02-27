d1f8a2378dfdcc1364d1534b10b78e1cf70c2b86
Harald Koch <harald.koch@gmail.com>
2022-02-27 11:19:05 -0500

    remove longhorn detritus

--- cluster/apps/default/longhorn-test/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CURRENT_DATE=$(date +"%FT%H%M")
-
-echo "running: mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS --quote-names --databases $DB_DATABASE \>/backup/${DB_DATABASE}.${CURRENT_DATE}.sql"
-mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS --quote-names --databases $DB_DATABASE >/backup/${DB_DATABASE}.${CURRENT_DATE}.sql
-
-echo "Complete."
-/bin/ls -l /backup
