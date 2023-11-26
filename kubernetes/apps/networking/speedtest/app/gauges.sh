0d71ad952f79314d1f635d614a656dc7d78318f6
Harald Koch <harald.koch@gmail.com>
2023-11-26 11:08:10 -0500

    chore(speedtest): examples moved

--- kubernetes/apps/networking/speedtest/app/gauges.sh
+++ kubernetes/apps/networking/speedtest/app/gauges.sh
@@ -3,4 +3,4 @@
 # This script overwrites the default (example-singleServer-full.html) page
 # with the pretty version.
 
-cat /config/www/example-singleServer-gauges.html | sed -e 's/LibreSpeed Example/Koch Haus Speed Test/' > /config/www/index.html
+cat /app/www/public/examples/example-singleServer-gauges.html | sed -e 's/LibreSpeed Example/Koch Haus Speed Test/' > /app/www/public/index.html
