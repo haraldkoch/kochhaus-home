9369292120f8374c76377eb6d3c1d71f475ee070
Harald Koch <harald.koch@gmail.com>
2025-02-04 09:10:02 -0500

    chore(opnsense): backup to NFS instead of S3

--- kubernetes/main/apps/network/opnsense/app/resources/opnsense-backup.sh
+++ kubernetes/main/apps/network/opnsense/app/resources/opnsense-backup.sh
@@ -3,17 +3,8 @@
 set -o nounset
 set -o errexit
 
-config_filename="$(date "+%Y%m%d-%H%M%S").xml"
-
-http_host=${S3_URL#*//}
-http_host=${http_host%:*}
-http_request_date=$(date -R)
-http_filepath="opnsense-backup/${config_filename}"
-http_signature=$(
-    printf "PUT\n\ntext/xml\n%s\n/%s" "${http_request_date}" "${http_filepath}" \
-        | openssl dgst -sha1 -hmac "${AWS_SECRET_ACCESS_KEY}" -binary \
-        | base64
-)
+# we rely on ZFS snapshots for versioning
+config_filename="opnsense-backup.xml"
 
 echo "Download Opnsense config file ..."
 curl -fsSL \
@@ -23,11 +14,5 @@ curl -fsSL \
 
 /bin/ls -l "/tmp/${config_filename}"
 
-echo "Upload backup to s3 bucket ..."
-curl -fsSL \
-    -X PUT -T "/tmp/${config_filename}" \
-    -H "Host: ${http_host}" \
-    -H "Date: ${http_request_date}" \
-    -H "Content-Type: text/xml" \
-    -H "Authorization: AWS ${AWS_ACCESS_KEY_ID}:${http_signature}" \
-    "${S3_URL}/${http_filepath}"
+echo "Save backup to disk ..."
+cp "/tmp/${config_filename}" "/data/${config_filename}"
