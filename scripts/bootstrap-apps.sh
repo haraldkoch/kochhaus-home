dadf614a248b72babb3af0910bdc3b495858110f
Harald Koch <harald.koch@gmail.com>
2025-03-02 14:47:40 -0500

    add talos and bootstrap

---
+++ scripts/bootstrap-apps.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+source "$(dirname "${0}")/lib/common.sh"
+
+export LOG_LEVEL="debug"
+export ROOT_DIR="$(git rev-parse --show-toplevel)"
+
+cluster=$1
+#FIXME validate cluster
+
+# Talos requires the nodes to be 'Ready=False' before applying resources
+function wait_for_nodes() {
+    log debug "Waiting for nodes to be available"
+
+    # Skip waiting if all nodes are 'Ready=True'
+    if kubectl --context ${cluster} wait nodes --for=condition=Ready=True --all --timeout=10s &>/dev/null; then
+        log info "Nodes are available and ready, skipping wait for nodes"
+        return
+    fi
+
+    # Wait for all nodes to be 'Ready=False'
+    until kubectl --context ${cluster} wait nodes --for=condition=Ready=False --all --timeout=10s &>/dev/null; do
+        log info "Nodes are not available, waiting for nodes to be available. Retrying in 10 seconds..."
+        sleep 10
+    done
+}
+
+# The application namespaces are created before applying the resources
+function apply_namespaces() {
+    log debug "Applying namespaces"
+
+    local -r apps_dir="${ROOT_DIR}/kubernetes/${cluster}/apps"
+
+    if [[ ! -d "${apps_dir}" ]]; then
+        log error "Directory does not exist" "directory=${apps_dir}"
+    fi
+
+    for app in "${apps_dir}"/*/; do
+        namespace=$(basename "${app}")
+
+        # Check if the namespace resources are up-to-date
+        if kubectl --context ${cluster} get namespace "${namespace}" &>/dev/null; then
+            log info "Namespace resource is up-to-date" "resource=${namespace}"
+            continue
+        fi
+
+        # Apply the namespace resources
+        if kubectl --context ${cluster} create namespace "${namespace}" --dry-run=client --output=yaml \
+            | kubectl --context ${cluster} apply --server-side --filename - &>/dev/null;
+        then
+            log info "Namespace resource applied" "resource=${namespace}"
+        else
+            log error "Failed to apply namespace resource" "resource=${namespace}"
+        fi
+    done
+}
+
+# ConfigMaps to be applied before the helmfile charts are installed
+function apply_configmaps() {
+    log debug "Applying ConfigMaps"
+
+    local -r configmaps=(
+        "${ROOT_DIR}/kubernetes/${cluster}/components/common/cluster-settings.yaml"
+    )
+
+    for configmap in "${configmaps[@]}"; do
+        if [ ! -f "${configmap}" ]; then
+            log warn "File does not exist" file "${configmap}"
+            continue
+        fi
+
+        # Check if the configmap resources are up-to-date
+        if kubectl --context ${cluster} --namespace flux-system diff --filename "${configmap}" &>/dev/null; then
+            log info "ConfigMap resource is up-to-date" "resource=$(basename "${configmap}" ".yaml")"
+            continue
+        fi
+
+        # Apply configmap resources
+        if kubectl --context ${cluster} --namespace flux-system apply --server-side --filename "${configmap}" &>/dev/null; then
+            log info "ConfigMap resource applied successfully" "resource=$(basename "${configmap}" ".yaml")"
+        else
+            log error "Failed to apply ConfigMap resource" "resource=$(basename "${configmap}" ".yaml")"
+        fi
+    done
+}
+
+# SOPS secrets to be applied before the helmfile charts are installed
+function apply_sops_secrets() {
+    log debug "Applying secrets"
+
+    local -r secrets=(
+        "${ROOT_DIR}/bootstrap/${cluster}/github-deploy-key.sops.yaml"
+        "${ROOT_DIR}/kubernetes/${cluster}/components/common/cluster-secrets.sops.yaml"
+        "${ROOT_DIR}/kubernetes/${cluster}/components/common/sops-age.sops.yaml"
+    )
+
+    for secret in "${secrets[@]}"; do
+        if [ ! -f "${secret}" ]; then
+            log warn "File does not exist" "file=${secret}"
+            continue
+        fi
+
+        # Check if the secret resources are up-to-date
+        if sops exec-file "${secret}" "kubectl --context ${cluster} --namespace flux-system diff --filename {}" &>/dev/null; then
+            log info "Secret resource is up-to-date" "resource=$(basename "${secret}" ".sops.yaml")"
+            continue
+        fi
+
+        # Apply secret resources
+        if sops exec-file "${secret}" "kubectl --context ${cluster} --namespace flux-system apply --server-side --filename {}" &>/dev/null; then
+            log info "Secret resource applied successfully" "resource=$(basename "${secret}" ".sops.yaml")"
+        else
+            log error "Failed to apply secret resource" "resource=$(basename "${secret}" ".sops.yaml")"
+        fi
+    done
+}
+
+# Apply Helm releases using helmfile
+function apply_helm_releases() {
+    log debug "Applying Helm releases with helmfile"
+
+    local -r helmfile_file="${ROOT_DIR}/bootstrap/${cluster}/helmfile.yaml"
+
+    if [[ ! -f "${helmfile_file}" ]]; then
+        log error "File does not exist" "file=${helmfile_file}"
+    fi
+
+    if ! helmfile --kube-context ${cluster} --file "${helmfile_file}" apply --hide-notes --skip-diff-on-install --suppress-diff --suppress-secrets; then
+        log error "Failed to apply Helm releases"
+    fi
+
+    log info "Helm releases applied successfully"
+}
+
+function main() {
+    check_cli helmfile kubectl kustomize sops talhelper yq
+
+    # Apply resources and Helm releases
+    wait_for_nodes
+    apply_namespaces
+    apply_configmaps
+    apply_sops_secrets
+    apply_helm_releases
+
+    log info "Congrats! The cluster is bootstrapped and Flux is syncing the Git repository"
+}
+
+main "$@"
