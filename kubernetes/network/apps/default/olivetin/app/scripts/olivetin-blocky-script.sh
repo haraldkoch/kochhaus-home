d74474322d6d2a04efa3156a9138969a24beef78
Harald Koch <harald.koch@gmail.com>
2025-08-20 18:12:00 -0400

    fix blocky script

--- kubernetes/network/apps/default/olivetin/app/scripts/olivetin-blocky-script.sh
+++ kubernetes/network/apps/default/olivetin/app/scripts/olivetin-blocky-script.sh
@@ -4,23 +4,19 @@ ACTION=$1
 DURATION=$2
 BLOCKY_GROUPS=$3 # this should be not required with v0.24, but it is now. use 'ads'
 
-for pod in $BLOCKY_PODS; do
+for host in castor pollux; do
     case "$ACTION" in
         status)
-            echo -n "castor: " && ssh castor /usr/bin/blocky blocking status
-            echo -n "pollux: " && ssh pollux /usr/bin/blocky blocking status
+            echo -n "${host}: " && ssh ${host} /usr/bin/blocky blocking status
         ;;
         enable)
-            echo -n "castor: " && ssh castor /usr/bin/blocky blocking enable
-            echo -n "pollux: " && ssh pollux /usr/bin/blocky blocking enable
+            echo -n "${host}: " && ssh ${host} /usr/bin/blocky blocking enable
         ;;
         disable)
             if [ -z "$DURATION" ]; then
-                echo -n "castor: " && ssh castor /usr/bin/blocky blocking disable --groups "$BLOCKY_GROUPS"
-                echo -n "pollux: " && ssh pollux /usr/bin/blocky blocking disable --groups "$BLOCKY_GROUPS"
+                echo -n "${host}: " && ssh ${host} /usr/bin/blocky blocking disable --groups "$BLOCKY_GROUPS"
             else
-                echo -n "castor: " && ssh castor /usr/bin/blocky blocking disable --duration "$DURATION" --groups "$BLOCKY_GROUPS"
-                echo -n "pollux: " && ssh pollux /usr/bin/blocky blocking disable --duration "$DURATION" --groups "$BLOCKY_GROUPS"
+                echo -n "${host}: " && ssh ${host} /usr/bin/blocky blocking disable --duration "$DURATION" --groups "$BLOCKY_GROUPS"
             fi
         ;;
     esac
