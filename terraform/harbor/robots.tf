35511941776010bd37b25d362fc9d37cff3dbd5a
Harald Koch <harald.koch@gmail.com>
2026-06-22 13:48:19 -0400

    add remaining robot accounts

--- terraform/harbor/robots.tf
+++ terraform/harbor/robots.tf
@@ -31,58 +31,58 @@ resource "harbor_robot_account" "containers_ci" {
   }
 }
 
-# resource "harbor_robot_account" "homeops_ci" {
-#   name        = "home-ops-ci"
-#   description = "CI push token for project 'home-ops' (Flux OCI sync)"
-#   level       = "project"
-#   duration    = -1
+resource "harbor_robot_account" "homeops_ci" {
+  name        = "home-ops-ci"
+  description = "CI push token for project 'home-ops' (Flux OCI sync)"
+  level       = "project"
+  duration    = -1
 
-#   permissions {
-#     access {
-#       action   = "push"
-#       resource = "repository"
-#     }
-#     access {
-#       action   = "pull"
-#       resource = "repository"
-#     }
-#     kind      = "project"
-#     namespace = harbor_project.private["home-ops"].name
-#   }
-# }
+  permissions {
+    access {
+      action   = "push"
+      resource = "repository"
+    }
+    access {
+      action   = "pull"
+      resource = "repository"
+    }
+    kind      = "project"
+    namespace = harbor_project.private["home-ops"].name
+  }
+}
 
-# resource "harbor_robot_account" "cluster_puller" {
-#   name        = "cluster-puller"
-#   description = "Cluster-wide pull token for Flux OCIRepository and pod imagePullSecrets"
-#   level       = "system"
-#   duration    = -1
+resource "harbor_robot_account" "cluster_puller" {
+  name        = "cluster-puller"
+  description = "Cluster-wide pull token for Flux OCIRepository and pod imagePullSecrets"
+  level       = "system"
+  duration    = -1
 
-#   permissions {
-#     access {
-#       action   = "pull"
-#       resource = "repository"
-#     }
-#     kind      = "project"
-#     namespace = "*"
-#   }
-# }
+  permissions {
+    access {
+      action   = "pull"
+      resource = "repository"
+    }
+    kind      = "project"
+    namespace = "*"
+  }
+}
 
-# resource "harbor_robot_account" "kochhaus_ci" {
-#   name        = "kochhaus-ci"
-#   description = "CI push token for project 'kochhaus' (signed OCI release artifacts: binaries + SBOM)"
-#   level       = "system"
-#   duration    = -1
+resource "harbor_robot_account" "kochhaus_ci" {
+  name        = "kochhaus-ci"
+  description = "CI push token for project 'kochhaus' (signed OCI release artifacts: binaries + SBOM)"
+  level       = "system"
+  duration    = -1
 
-#   permissions {
-#     access {
-#       action   = "push"
-#       resource = "repository"
-#     }
-#     access {
-#       action   = "pull"
-#       resource = "repository"
-#     }
-#     kind      = "project"
-#     namespace = harbor_project.private["kochhaus"].name
-#   }
-# }
+  permissions {
+    access {
+      action   = "push"
+      resource = "repository"
+    }
+    access {
+      action   = "pull"
+      resource = "repository"
+    }
+    kind      = "project"
+    namespace = harbor_project.private["kochhaus"].name
+  }
+}
--- terraform/harbor/secrets.tf
+++ terraform/harbor/secrets.tf
@@ -19,62 +19,65 @@ resource "onepassword_item" "harbor_containers_ci" {
   }
 }
 
-# resource "infisical_secret" "homeops_ci_name" {
-#   name         = "HARBOR_ROBOT_NAME"
-#   value        = harbor_robot_account.homeops_ci.full_name
-#   env_slug     = "prod"
-#   workspace_id = var.infisical_workspace_id
-#   folder_path  = "/kubernetes/harbor/robots/home-ops"
+resource "onepassword_item" "harbor_homeops_ci" {
+  vault = var.onepassword_vault_id
+  title = "harbor-homeops-ci"
+  category = "secure_note"
 
-#   depends_on = [infisical_secret_folder.robot]
-# }
+  section_map = {
+    "robots" = {
+      field_map = {
+        "username" = {
+          type = "STRING"
+          value = harbor_robot_account.homeops_ci.full_name
+        }
+        "password" = {
+          type = "CONCEALED"
+          value = harbor_robot_account.homeops_ci.secret
+        }
+      }
+    }
+  }
+}
 
-# resource "infisical_secret" "homeops_ci_token" {
-#   name         = "HARBOR_ROBOT_TOKEN"
-#   value        = harbor_robot_account.homeops_ci.secret
-#   env_slug     = "prod"
-#   workspace_id = var.infisical_workspace_id
-#   folder_path  = "/kubernetes/harbor/robots/home-ops"
+resource "onepassword_item" "harbor_cluster_puller" {
+  vault = var.onepassword_vault_id
+  title = "harbor-cluster-puller"
+  category = "secure_note"
 
-#   depends_on = [infisical_secret_folder.robot]
-# }
+  section_map = {
+    "robots" = {
+      field_map = {
+        "username" = {
+          type = "STRING"
+          value = harbor_robot_account.cluster_puller.full_name
+        }
+        "password" = {
+          type = "CONCEALED"
+          value = harbor_robot_account.cluster_puller.secret
+        }
+      }
+    }
+  }
+}
 
-# resource "infisical_secret" "cluster_puller_name" {
-#   name         = "HARBOR_ROBOT_NAME"
-#   value        = harbor_robot_account.cluster_puller.full_name
-#   env_slug     = "prod"
-#   workspace_id = var.infisical_workspace_id
-#   folder_path  = "/kubernetes/harbor/robots/cluster-puller"
+resource "onepassword_item" "harbor_kochhaus_ci" {
+  vault = var.onepassword_vault_id
+  title = "harbor-kochhaus-ci"
+  category = "secure_note"
 
-#   depends_on = [infisical_secret_folder.robot]
-# }
-
-# resource "infisical_secret" "cluster_puller_token" {
-#   name         = "HARBOR_ROBOT_TOKEN"
-#   value        = harbor_robot_account.cluster_puller.secret
-#   env_slug     = "prod"
-#   workspace_id = var.infisical_workspace_id
-#   folder_path  = "/kubernetes/harbor/robots/cluster-puller"
-
-#   depends_on = [infisical_secret_folder.robot]
-# }
-
-# resource "infisical_secret" "kochhaus_ci_name" {
-#   name         = "HARBOR_ROBOT_NAME"
-#   value        = harbor_robot_account.kochhaus_ci.full_name
-#   env_slug     = "prod"
-#   workspace_id = var.infisical_workspace_id
-#   folder_path  = "/kubernetes/harbor/robots/kochhaus"
-
-#   depends_on = [infisical_secret_folder.robot]
-# }
-
-# resource "infisical_secret" "kochhaus_ci_token" {
-#   name         = "HARBOR_ROBOT_TOKEN"
-#   value        = harbor_robot_account.kochhaus_ci.secret
-#   env_slug     = "prod"
-#   workspace_id = var.infisical_workspace_id
-#   folder_path  = "/kubernetes/harbor/robots/kochhaus"
-
-#   depends_on = [infisical_secret_folder.robot]
-# }
+  section_map = {
+    "robots" = {
+      field_map = {
+        "username" = {
+          type = "STRING"
+          value = harbor_robot_account.kochhaus_ci.full_name
+        }
+        "password" = {
+          type = "CONCEALED"
+          value = harbor_robot_account.kochhaus_ci.secret
+        }
+      }
+    }
+  }
+}
