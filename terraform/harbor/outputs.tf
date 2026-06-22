17b15ed605372c318e8a7f7082b95fdd8ec4e0c4
Harald Koch <harald.koch@gmail.com>
2026-06-22 15:05:42 -0400

    chore: restore missing outputs

--- terraform/harbor/outputs.tf
+++ terraform/harbor/outputs.tf
@@ -2,13 +2,13 @@ output "harbor_url" {
   value = var.harbor_url
 }
 
-# output "private_projects" {
-#   value = [for p in harbor_project.private : p.name]
-# }
+output "private_projects" {
+  value = [for p in harbor_project.private : p.name]
+}
 
-# output "proxy_projects" {
-#   value = {
-#     for k, p in harbor_project.proxy :
-#     k => "${trimprefix(var.harbor_url, "https://")}/${p.name}"
-#   }
-# }
+output "proxy_projects" {
+  value = {
+    for k, p in harbor_project.proxy :
+    k => "${trimprefix(var.harbor_url, "https://")}/${p.name}"
+  }
+}
